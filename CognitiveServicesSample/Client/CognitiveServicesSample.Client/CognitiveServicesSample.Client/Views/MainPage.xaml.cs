6d50b74322171ab0a779e36232f979e9507646b4
Kazuki Ota <kaota@microsoft.com>
2017-05-29 10:30:47 +0900

    作業土台作成

---
+++ CognitiveServicesSample/Client/CognitiveServicesSample.Client/CognitiveServicesSample.Client/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CognitiveServicesSample.Client.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
