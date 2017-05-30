ebca6be3f4d89261087040ff4d69eb180635b038
Kazuki Ota <kaota@microsoft.com>
2017-05-30 13:23:12 +0900

    分析を追加

--- src/Server/CognitiveServiceSample.Jobs/Program.cs
+++ src/Server/CognitiveServiceSample.Jobs/Program.cs
@@ -4,26 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using CognitiveServiceSample.Jobs.Services;
+using System.Configuration;
+using CognitiveServicesSample.Data;
+using Microsoft.Extensions.Options;
 
 namespace CognitiveServiceSample.Jobs
 {
-    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
-            var config = new JobHostConfiguration();
-
-            if (config.IsDevelopment)
+            // settings
+            var translatorSetting = new TranslatorSetting
             {
-                config.UseDevelopmentSettings();
-            }
+                APIKey = ConfigurationManager.AppSettings["TranslatorAPIKey"],
+            };
+            var twitterSetting = new TwitterSetting
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"],
+                ConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"],
+                AccessToken = ConfigurationManager.AppSettings["TwitterAccessToken"],
+                AccessTokenSecret = ConfigurationManager.AppSettings["TwitterAccessTokenSecret"],
+            };
+            var visionSetting = new VisionSetting
+            {
+                APIKey = ConfigurationManager.AppSettings["VisionAPIAPIKey"],
+                Endpoint = ConfigurationManager.AppSettings["VisionAPIEndpoint"],
+            };
+            var cosmosDbSetting = new CosmosDbSetting
+            {
+                EndpointUri = ConfigurationManager.AppSettings["CosmosDbEndpointUri"],
+                PrimaryKey = ConfigurationManager.AppSettings["CosmosDbPrimaryKey"],
+            };
+            var analyzeSetting = new AnalyzeSetting
+            {
+                Keyword = ConfigurationManager.AppSettings["AnalyzeKeyword"],
+            };
 
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
+            // services
+            var translatorService = new TranslatorService(Options.Create(translatorSetting));
+            var twitterService = new TwitterService(Options.Create(twitterSetting));
+            var visionService = new VisionService(Options.Create(visionSetting), translatorService);
+            var categolizedImageRepository = new CategolizedImageRepository(Options.Create(cosmosDbSetting));
+            var analyzeService = new AnalyzeService(Options.Create(analyzeSetting),
+                twitterService,
+                visionService,
+                categolizedImageRepository);
+
+            // run
+            analyzeService.AnalyzeAsync().Wait();
         }
     }
 }
