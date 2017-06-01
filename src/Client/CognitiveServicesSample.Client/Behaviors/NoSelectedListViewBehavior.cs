8b949548956136061f99d41c8fefe66a0f3e1c9d
Kazuki Ota <kaota@microsoft.com>
2017-06-01 17:53:45 +0900

    ListViewを選択不可能にした

---
+++ src/Client/CognitiveServicesSample.Client/Behaviors/NoSelectedListViewBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CognitiveServicesSample.Client.Behaviors
+{
+    public class NoSelectedListViewBehavior : Behavior<ListView>
+    {
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.ItemSelected += this.Bindable_ItemSelected;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.ItemSelected -= this.Bindable_ItemSelected;
+        }
+
+        private void Bindable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+
+            ((ListView)sender).SelectedItem = null;
+        }
+
+    }
+}
